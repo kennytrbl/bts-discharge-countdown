1f78496c7283578a3b162144772ad77745bb5c3f
kennytrbl <kennyzhang418@gmail.com>
2024-07-25 16:51:00 -0700

    local push

--- src/components/Info.js
+++ src/components/Info.js
@@ -1,113 +1,115 @@
 import React, { Component } from "react";
 import Row from "./Row.js";
 
-// for future reference: use nested arrays for better data visualization
-const name = ["Jin", "J-Hope", "Suga", "RM", "V", "Jimin", "Jungkook"];
-const profilePic = [];
+// Member data
+const members = [
+  {
+    name: "Jin",
+    realName: "Kim Seok-jin",
+    enlistmentDate: "December 13, 2022",
+    dischargeDate: "June 12, 2024",
+    instagramLink: "https://www.instagram.com/jin",
+    spotifyLink: "https://open.spotify.com/artist/5vV3bFXnN6D6N3Nj4xRvaV",
+    tiktokLink: "",
+  },
+  {
+    name: "J-Hope",
+    realName: "Jung Ho-seok",
+    enlistmentDate: "April 18, 2023",
+    dischargeDate: "October 17, 2024",
+    instagramLink: "https://www.instagram.com/uarmyhope",
+    spotifyLink: "https://open.spotify.com/artist/0b1sIQumIAsNbqAoIClSpy",
+    tiktokLink: "",
+  },
+  {
+    name: "Suga",
+    realName: "Min Yoon-gi",
+    enlistmentDate: "September 22, 2023",
+    dischargeDate: "June 21, 2025",
+    instagramLink: "https://www.instagram.com/agustd",
+    spotifyLink: "https://open.spotify.com/artist/0ebNdVaOfp6N0oZ1guIxM8",
+    tiktokLink: "",
+  },
+  {
+    name: "RM",
+    realName: "Kim Nam-joon",
+    enlistmentDate: "December 11, 2023",
+    dischargeDate: "June 10, 2025",
+    instagramLink: "https://www.instagram.com/rkive",
+    spotifyLink: "https://open.spotify.com/artist/2auC28zjQyVTsiZKNgPRGs",
+    tiktokLink: "",
+  },
+  {
+    name: "V",
+    realName: "Kim Tae-hyung",
+    enlistmentDate: "December 11, 2023",
+    dischargeDate: "June 10, 2025",
+    instagramLink: "https://www.instagram.com/thv",
+    spotifyLink: "https://open.spotify.com/artist/3JsHnjpbhX4SnySpvpa9DK",
+    tiktokLink: "",
+  },
+  {
+    name: "Jimin",
+    realName: "Park Ji-min",
+    enlistmentDate: "December 12, 2023",
+    dischargeDate: "June 11, 2025",
+    instagramLink: "https://www.instagram.com/j.m",
+    spotifyLink: "https://open.spotify.com/artist/1oSPZhvZMIrWW5I41kPkkY",
+    tiktokLink: "",
+  },
+  {
+    name: "Jungkook",
+    realName: "Jeon Jung-kook",
+    enlistmentDate: "December 12, 2023",
+    dischargeDate: "June 11, 2025",
+    instagramLink: "",
+    spotifyLink: "https://open.spotify.com/artist/6HaGTQPmzraVmaVxvz6EUc",
+    tiktokLink: "https://www.tiktok.com/@jungkook",
+  },
+];
 
-for (let i = 0; i < name.length; i++) {
-  if (name[i] === "Jungkook") {
-    profilePic.push(`https://ibighit.com/bts/images/profile/proof/member/member-jk.jpg`);
-  } else if (name[i] === "J-Hope") {
-    profilePic.push(
-      `https://ibighit.com/bts/images/profile/proof/member/member-${name[i].toLowerCase().replace("-", "")}.jpg`,
-    );
-  } else {
-    profilePic.push(`https://ibighit.com/bts/images/profile/proof/member/member-${name[i].toLowerCase()}.jpg`);
+const profilePicBaseUrl = "https://ibighit.com/bts/images/profile/proof/member/";
+
+const profilePicUrls = members.map((member) => {
+  if (member.name === "Jungkook") {
+    return "https://ibighit.com/bts/images/profile/proof/member/member-jk.jpg";
   }
-}
-
-const realName = [
-  "Kim Seok-jin",
-  "Jung Ho-seok",
-  "Min Yoon-gi",
-  "Kim Nam-joon",
-  "Kim Tae-hyung",
-  "Park Ji-min",
-  "Jeon Jung-kook",
-];
-
-const enlistmentDate = [
-  "December 13, 2022",
-  "April 18, 2023",
-  "September 22, 2023",
-  "December 11, 2023",
-  "December 11, 2023",
-  "December 12, 2023",
-  "December 12, 2023",
-];
-
-const dischargeDate = [
-  "June 12, 2024",
-  "October 17, 2024",
-  "June 21, 2025",
-  "June 10, 2025",
-  "June 10, 2025",
-  "June 11, 2025",
-  "June 11, 2025",
-];
-
-let daysLeft = [
-  calculateDaysLeft("June 12, 2024"),
-  calculateDaysLeft("October 17, 2024"),
-  calculateDaysLeft("June 21, 2025"),
-  calculateDaysLeft("June 10, 2025"),
-  calculateDaysLeft("June 10, 2025"),
-  calculateDaysLeft("June 11, 2025"),
-  calculateDaysLeft("June 11, 2025"),
-];
-
-const instagramLink = [
-  "https://www.instagram.com/jin",
-  "https://www.instagram.com/uarmyhope",
-  "https://www.instagram.com/agustd",
-  "https://www.instagram.com/rkive",
-  "https://www.instagram.com/thv",
-  "https://www.instagram.com/j.m",
-  "",
-];
-
-const spotifyLink = [
-  "https://open.spotify.com/artist/5vV3bFXnN6D6N3Nj4xRvaV",
-  "https://open.spotify.com/artist/0b1sIQumIAsNbqAoIClSpy",
-  "https://open.spotify.com/artist/0ebNdVaOfp6N0oZ1guIxM8",
-  "https://open.spotify.com/artist/2auC28zjQyVTsiZKNgPRGs",
-  "https://open.spotify.com/artist/3JsHnjpbhX4SnySpvpa9DK",
-  "https://open.spotify.com/artist/1oSPZhvZMIrWW5I41kPkkY",
-  "https://open.spotify.com/artist/6HaGTQPmzraVmaVxvz6EUc",
-];
-
-const tiktokLink = ["", "", "", "", "", "", "https://www.tiktok.com/@jungkook"];
+  const name = member.name.toLowerCase().replace("-", "");
+  return `${profilePicBaseUrl}member-${name}.jpg`;
+});
 
 function calculateDaysLeft(dischargeDate) {
-  var distance = new Date(dischargeDate).getTime() - new Date().getTime();
-  var days = Math.ceil(Math.floor(distance / (1000 * 60 * 60 * 24)));
-  if (days === 0 || days < 0) {
-    return 0;
-  }
-  return days;
+  const distance = new Date(dischargeDate).getTime() - new Date().getTime();
+  const days = Math.ceil(distance / (1000 * 60 * 60 * 24));
+  return days <= 0 ? "Discharged from the military" : days;
 }
 
-let array = [];
-for (let i = 0; i < realName.length; i++) {
-  array.push(
-    <Row
-      profilePic={profilePic[i]}
-      name={name[i]}
-      realName={realName[i]}
-      enlistmentDate={enlistmentDate[i]}
-      dischargeDate={dischargeDate[i]}
-      daysLeft={daysLeft[i]}
-      instagramLink={instagramLink[i]}
-      spotifyLink={spotifyLink[i]}
-      tiktokLink={tiktokLink[i]}
-    />,
-  );
-}
+const membersWithDaysLeft = members.map((member, index) => ({
+  ...member,
+  profilePic: profilePicUrls[index],
+  daysLeft: calculateDaysLeft(member.dischargeDate),
+}));
 
 class Info extends Component {
   render() {
-    return <div className="container">{array}</div>;
+    return (
+      <div className="container">
+        {membersWithDaysLeft.map((member, index) => (
+          <Row
+            key={index}
+            profilePic={member.profilePic}
+            name={member.name}
+            realName={member.realName}
+            enlistmentDate={member.enlistmentDate}
+            dischargeDate={member.dischargeDate}
+            daysLeft={member.daysLeft}
+            instagramLink={member.instagramLink}
+            spotifyLink={member.spotifyLink}
+            tiktokLink={member.tiktokLink}
+          />
+        ))}
+      </div>
+    );
   }
 }
 
